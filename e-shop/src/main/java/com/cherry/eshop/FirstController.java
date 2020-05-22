b7bd665d03aa64553ced903f3b2e96c04b3ea23e
Yorologo <esaud@hotmail.com>
2020-05-22 01:37:08 -0500

    Muestra el contenido de la DB en una estructura erronea

--- e-shop/src/main/java/com/cherry/eshop/FirstController.java
+++ e-shop/src/main/java/com/cherry/eshop/FirstController.java
@@ -18,7 +18,28 @@ public class FirstController {
 	@RequestMapping(value = "/greeting", method = RequestMethod.POST)
 	public String greeting(Model model) {
 		List<Producto> productos = ProductoDao.showAll();
-		model.addAttribute("productos", productos);
+		String content = "";
+		int count = 0;
+		for (Producto producto : productos) {
+			if (count % 4 == 0)
+				content += "<div class=\"row d-flex flex-row justify-content m-3\">";
+			content += "<div class=\"col\"><div class=\"card\" style=\"width: 18rem;\"><img src=\"img/"
+					+ producto.getImagen()
+					+ "\" class=\"card-img-top\" alt=\"...\"><div class=\"card-body bg-info text-white text-center\"><h5 class=\"card-title\">"
+					+ producto.getNombre() + "</h5><br><label class=\"col col-form-label\">$ " + producto.getPrecio()
+					+ "</label><div class=\"form-group row\"><label class=\"col col-form-label\">Cantidad</label><div class=\"col\"><input type=\"number\" id=\"cantidad"
+					+ count
+					+ "\" class=\"form-control\" min=\"0\" step=\"1\" value=\"0\"></div></div></div></div></div>";
+			if (count % 4 - 1 == 0)
+				content += "</div>";
+			count++;
+		}
+		for (int i = count % 4; i < 4 && i != 0; i++) {
+			content += "<div class=\"col\"></div>";
+			if (i == 3)
+				content += "</div>";
+		}
+		model.addAttribute("content", content);
 		return "greeting";
 	}
 
